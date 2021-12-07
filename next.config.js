bccfe8c689a835703cac74617506ce0b2e1166ad
TRONG <tronginc@gmail.com>
2021-12-07 21:46:32 +0700

    Release private-pack

--- next.config.js
+++ next.config.js
@@ -13,16 +13,27 @@ module.exports = {
   },
   publicRuntimeConfig: {
     supportedMetaMaskNetworks: [
+      // {
+      //   chainId: "0x61",
+      //   chainName: "BSC Testnet",
+      //   nativeCurrency: {
+      //     name: "BNB",
+      //     symbol: "BNB",
+      //     decimals: 18,
+      //   },
+      //   rpcUrls: ["https://data-seed-prebsc-1-s1.binance.org:8545/"],
+      //   blockExplorerUrls: ["https://testnet.bscscan.com"],
+      // },
       {
-        chainId: "0x61",
-        chainName: "BSC Testnet",
+        chainId: "0x38",
+        chainName: "Smart Chain - Main Network",
         nativeCurrency: {
-          name: "BSC",
-          symbol: "BSC",
+          name: "BNB",
+          symbol: "BNB",
           decimals: 18,
         },
-        rpcUrls: ["https://data-seed-prebsc-1-s1.binance.org:8545/"],
-        blockExplorerUrls: ["https://testnet.bscscan.com"],
+        rpcUrls: ["https://bsc-dataseed.binance.org/"],
+        blockExplorerUrls: ["https://bscscan.com"],
       },
     ],
   },
