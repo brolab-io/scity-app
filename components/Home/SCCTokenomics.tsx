c86b603914b68f18296dfd30d45c0cfe67b11cad
TRONG <tronginc@gmail.com>
2021-12-14 23:39:56 +0700

    sCC Tokenomics for Home Page

--- components/Home/SCCTokenomics.tsx
+++ components/Home/SCCTokenomics.tsx
@@ -5,27 +5,21 @@ import styles from "./SCCTokenomics.module.css";
 const SCCTokenomics = () => {
   return (
     <Container className="px-4 py-20 mt-20 md:px-6 lg:px-8 xl:px-10">
-      <div className="space-y-6 text-center">
-        <h3 className="space-x-4 text-xl font-extrabold lg:text-2xl xl:text-3xl 2xl:text-4xl">
+      <div className="space-y-8 text-center">
+        <h3 className="space-x-4 text-xl font-bold text-[40px] uppercase">
           <span className="text-purple-600">SCC</span>
           <span className="text-white">Tokenomics</span>
         </h3>
-        <p className="mx-auto text-white w-80">
+        <p className="mx-auto text-white text-[16px]">
           We believe artists need to be compensated for every sale,not just the first one!
         </p>
       </div>
       <div
-        className={clsx(styles["total-supply-container"], "relative bg-150 sm:bg-125 lg:bg-100")}
-      >
-        <div className="absolute inset-0 flex flex-col items-center justify-center space-y-4">
-          <span className="block text-sm font-bold text-white sm:text-lg md:text-xl lg:text-2xl xl:text-4xl">
-            1.000.000.000 <span className="text-purple-700">SCC</span>
-          </span>
-          <span className="block text-lg font-bold text-gray-300 md:text-xl lg:text-2xl">
-            Total supply
-          </span>
-        </div>
-      </div>
+        className={clsx(
+          styles["total-supply-container"],
+          "relative bg-150 sm:bg-125 lg:bg-100 -mt-24 -mb-40"
+        )}
+      ></div>
     </Container>
   );
 };
