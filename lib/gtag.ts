0ec66daf76c6c8153fd54a7bbd6404c7a998dd65
Pham Hong Thai <hongthaipro@gmail.com>
2021-12-21 09:57:26 +0700

    chang gtag

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,17 +1,8 @@
-export const GA_TRACKING_ID = "GTM-TFV6XCN";
+export const GTM_ID = "GTM-TFV6XCN";
 
-// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: string) => {
-  (window as any).gtag("config", GA_TRACKING_ID, {
-    page_path: url,
-  });
-};
-
-// https://developers.google.com/analytics/devguides/collection/gtagjs/events
-export const event = ({ action, category, label, value }: any) => {
-  (window as any).gtag("event", action, {
-    event_category: category,
-    event_label: label,
-    value: value,
+  (window as any).dataLayer.push({
+    event: "pageview",
+    page: url,
   });
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -140,21 +140,24 @@ function MyApp({ Component, pageProps }: AppProps) {
       </Head>
       <Layout>
         <DefaultSeo {...SEO} />
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${gtag.GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-        />
-        <Script
-          id="gtag-init"
+          id="gtag"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${gtag.GTM_ID}');
           `,
           }}
         />
