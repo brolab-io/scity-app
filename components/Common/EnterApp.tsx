0a50abe7e9074d9255cba4e7dad6c36bc058ac05
TRONG <tronginc@gmail.com>
2022-01-15 01:39:39 +0700

    enter app staging

--- components/Common/EnterApp.tsx
+++ components/Common/EnterApp.tsx
@@ -1,19 +1,23 @@
 import styles from "./Connect.module.css";
 import clsx from "clsx";
+import Link from "next/link";
 
 const EnterApp: React.FC = () => {
+  const href = process.env["NEXT_PUBLIC_ENV"] === "production" ? "#" : "/marketplace";
   return (
-    <button
-      className={clsx(
-        styles["enter-app"],
-        "text-sm text-white px-10 py-[10px] rounded-[30px] flex items-center",
-        "hover:opacity-80 duration-200",
-        "disabled:opacity-60 duration-200",
-        "animate-scale relative"
-      )}
-    >
-      <span className="whitespace-nowrap font-medium uppercase text-[16px]">Enter App</span>
-    </button>
+    <Link passHref href={href}>
+      <a
+        className={clsx(
+          styles["enter-app"],
+          "text-sm text-white px-10 py-[10px] rounded-[30px] flex items-center",
+          "hover:opacity-80 duration-200",
+          "disabled:opacity-60 duration-200",
+          "animate-scale relative"
+        )}
+      >
+        <span className="whitespace-nowrap font-medium uppercase text-[16px]">Enter App</span>
+      </a>
+    </Link>
   );
 };
 
--- components/PrivateBox/Narbar.tsx
+++ components/PrivateBox/Narbar.tsx
@@ -10,7 +10,7 @@ const PrivateBoxNavbar = ({}) => {
   const { active, account } = useWeb3React<Web3Provider>();
   return (
     <div className="fixed top-0 left-0 right-0 z-30 flex items-center justify-between h-16 px-4 bg-black border-b border-gray-900 shadow-sm">
-      <Logo />
+      <Logo width={80} height={80} />
       <div>
         {active ? (
           <div className="relative flex-none group">
