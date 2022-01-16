ffc542b92b93cfbe7723464e1fb91e59d76e8993
TRONG <tronginc@gmail.com>
2022-01-16 21:21:38 +0700

    update staging

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "scity-app-staging",
-      script: "node_modules/next/dist/bin/next",
-      args: "start",
-      cwd: "./",
-      instances: "1",
-      exec_mode: "cluster",
-      max_memory_restart: "500M",
-      env: {
-        PORT: 3030,
-        ENV: "staging",
-      },
-    },
-    {
-      name: "scity-app-production",
+      name: "scity-app",
       script: "node_modules/next/dist/bin/next",
       args: "start",
       cwd: "./",
@@ -23,7 +10,6 @@ module.exports = {
       max_memory_restart: "500M",
       env: {
         PORT: 3000,
-        ENV: "production",
       },
     },
   ],
