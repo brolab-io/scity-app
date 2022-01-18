61980c2036c5099170665e6437eab630a1a241bd
TRONG <tronginc@gmail.com>
2022-01-18 19:49:28 +0700

    update comming soon text-color

--- components/Common/CommingSoon.tsx
+++ components/Common/CommingSoon.tsx
@@ -5,9 +5,9 @@ const CommingSoon: NextPage = () => {
   return (
     <div className="space-y-6 flex justify-center flex-col items-center pt-20">
       <Logo width={200} height={200} />
-      <h3 className="text-xl font-extrabold lg:text-2xl xl:text-3xl 2xl:text-4xl space-y-7 text-center">
-        <span className="text-purple-600 block">THE SOCIALVERSE CITY</span>
-        <span className="text-white block">Coming Soon .......</span>
+      <h3 className="text-xl font-extrabold lg:text-2xl xl:text-3xl 2xl:text-4xl space-y-5 text-center">
+        <span className="text-magenta block">THE SOCIALVERSE CITY</span>
+        <span className="text-[#A0AEC0] block">Coming Soon .......</span>
       </h3>
     </div>
   );
