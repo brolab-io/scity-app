4ad50212ca1b28e4f0c1086d920603cd95adea35
TRONG <tronginc@gmail.com>
2022-01-18 23:32:36 +0700

    add title to marketplace item

--- pages/marketplace/[id].tsx
+++ pages/marketplace/[id].tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import SvgChevronRight from "../../components/Icons/SvgChevronRight";
 import MarketPlaceRules from "../../components/MarketPlace/Rules";
 import { NextSeo } from "next-seo";
+import NewLayout from "../../components/UI/NewLayout";
 
 const images = [
   "https://res.cloudinary.com/dcrbaasbt/image/upload/v1640017575/ElSalvador_unj1az.png",
@@ -35,111 +36,107 @@ const NFTDetail: NextPage = () => {
   return (
     <>
       <NextSeo title="#12 Torontoce" />
-      <div className="bg-[#171923] pt-16 min-h-screen">
-        <Container className="px-4 pt-6 pb-10 space-y-6 md:pb-12 lg:pb-16 md:pt-10 lg:pt-14 lg:space-y-8 md:px-8 xl:px-4">
-          <div className="flex font-[16px] font-medium">
-            <Link href="/marketplace" passHref>
-              <a className="text-[#718096] hover:text-white">Marketplace</a>
-            </Link>
-            <SvgChevronRight className="w-6 h-6" />
-            <span className="text-white">#12 Torontoce</span>
-          </div>
-          <div className="grid gap-8 mt-10 lg:grid-cols-3 md:mt-12 lg:mt-14">
-            {/* #################### Image & NFT Info ####################*/}
-            <div className="col-span-2 bg-[#1A202C] rounded-[24px]">
-              <div className="grid gap-8 p-4 sm:grid-cols-2 md:p-6 lg:p-8">
-                <div
-                  className="relative aspect-[352/409] w-full items-center flex justify-center rounded-[16px]"
-                  style={bgStyles[index]}
-                >
-                  <Image
-                    quality={100}
-                    className="absolute"
-                    alt=""
-                    src={images[index]}
-                    height={242}
-                    width={177}
-                  />
-                </div>
-                <div className="space-y-8">
-                  <h1 className="text-white font-semibold text-[32px]">#12 Toronto</h1>
-                  <div className="space-y-[19px]">
-                    <div className="flex justify-between">
-                      <span className="text-[#718096] text-[16px]">Par Value</span>
-                      <span className="text-white text-[16px] font-medium">302.12 SCC</span>
-                    </div>
-                    <div className="flex justify-between">
-                      <span className="text-[#718096] text-[16px]">Mining Efficiency</span>
-                      <span className="text-white text-[16px] font-medium">200.45 %</span>
-                    </div>
-                    <div className="flex justify-between">
-                      <span className="text-[#718096] text-[16px]">Mining Power</span>
-                      <span className="text-white text-[16px] font-medium">120 SCC</span>
-                    </div>
-                    <div className="flex justify-between">
-                      <span className="text-[#718096] text-[16px]">Rarity</span>
-                      <span className="text-white text-[16px] font-medium">N</span>
-                    </div>
-                    <div className="bg-[#2D3748] rounded-[16px] p-4 space-y-4">
-                      <span className="text-[#A0AEC0]">Sale Price (SCC)</span>
-                      <span className="text-[20px] text-white font-medium space-x-6 flex items-center">
-                        <div className="flex items-center space-x-2">
-                          <Image
-                            quality={100}
-                            src="/assets/images/scc.png"
-                            alt="SCC"
-                            height={32}
-                            width={32}
-                          />
-                          <span>605.250</span>
-                        </div>
-                        <span className="text-[#A0AEC0] text-[16px]">~ $200.566</span>
-                      </span>
-                    </div>
+      <NewLayout title="Marketplace">
+        <div className="flex font-[16px] font-medium">
+          <Link href="/marketplace" passHref>
+            <a className="text-[#718096] hover:text-white">Marketplace</a>
+          </Link>
+          <SvgChevronRight className="w-6 h-6" />
+          <span className="text-white">#12 Torontoce</span>
+        </div>
+        <div className="grid gap-8 mt-10 lg:grid-cols-3 md:mt-12 lg:mt-14">
+          {/* #################### Image & NFT Info ####################*/}
+          <div className="col-span-2 bg-[#1A202C] rounded-[24px]">
+            <div className="grid gap-8 p-4 sm:grid-cols-2 md:p-6 lg:p-8">
+              <div
+                className="relative aspect-[352/409] w-full items-center flex justify-center rounded-[16px]"
+                style={bgStyles[index]}
+              >
+                <Image
+                  quality={100}
+                  className="absolute"
+                  alt=""
+                  src={images[index]}
+                  height={242}
+                  width={177}
+                />
+              </div>
+              <div className="space-y-8">
+                <h1 className="text-white font-semibold text-[32px]">#12 Toronto</h1>
+                <div className="space-y-[19px]">
+                  <div className="flex justify-between">
+                    <span className="text-[#718096] text-[16px]">Par Value</span>
+                    <span className="text-white text-[16px] font-medium">302.12 SCC</span>
                   </div>
-                  <button className="w-full rounded button button-magenta button-rounded">
-                    <span className="text-[12px] md:text-[14px] lg:text-[16px]">
-                      Connect Wallet
+                  <div className="flex justify-between">
+                    <span className="text-[#718096] text-[16px]">Mining Efficiency</span>
+                    <span className="text-white text-[16px] font-medium">200.45 %</span>
+                  </div>
+                  <div className="flex justify-between">
+                    <span className="text-[#718096] text-[16px]">Mining Power</span>
+                    <span className="text-white text-[16px] font-medium">120 SCC</span>
+                  </div>
+                  <div className="flex justify-between">
+                    <span className="text-[#718096] text-[16px]">Rarity</span>
+                    <span className="text-white text-[16px] font-medium">N</span>
+                  </div>
+                  <div className="bg-[#2D3748] rounded-[16px] p-4 space-y-4">
+                    <span className="text-[#A0AEC0]">Sale Price (SCC)</span>
+                    <span className="text-[20px] text-white font-medium space-x-6 flex items-center">
+                      <div className="flex items-center space-x-2">
+                        <Image
+                          quality={100}
+                          src="/assets/images/scc.png"
+                          alt="SCC"
+                          height={32}
+                          width={32}
+                        />
+                        <span>605.250</span>
+                      </div>
+                      <span className="text-[#A0AEC0] text-[16px]">~ $200.566</span>
                     </span>
-                  </button>
+                  </div>
                 </div>
+                <button className="w-full rounded button button-magenta button-rounded">
+                  <span className="text-[12px] md:text-[14px] lg:text-[16px]">Connect Wallet</span>
+                </button>
               </div>
             </div>
+          </div>
 
-            {/* #################### On-Chain Data ####################*/}
-            <div className="bg-[#1A202C] col-span-2 lg:col-span-1 p-6 rounded-[24px] space-y-8">
-              <h4 className="text-[24px] font-semibold text-white">On-Chain Data</h4>
-              <div className="space-y-[19px]">
-                <div className="flex justify-between">
-                  <span className="text-[#718096] text-[16px]">Owner</span>
-                  <span className="text-magenta text-[16px] font-medium">0x57CB...ea4a6</span>
-                </div>
-                <div className="flex justify-between">
-                  <span className="text-[#718096] text-[16px]">Contract Address</span>
-                  <span className="text-magenta text-[16px] font-medium">0x57CB...ea4a6</span>
-                </div>
-                <div className="flex justify-between">
-                  <span className="text-[#718096] text-[16px]">Token ID</span>
-                  <span className="text-white text-[16px] font-medium">#1223</span>
-                </div>
-                <div className="flex justify-between">
-                  <span className="text-[#718096] text-[16px]">Asset Protocol</span>
-                  <span className="text-white text-[16px] font-medium">BSC721</span>
-                </div>
-                <div className="flex justify-between">
-                  <span className="text-[#718096] text-[16px]">Asset Public Chain</span>
-                  <span className="text-white text-[16px] font-medium">BSC</span>
-                </div>
+          {/* #################### On-Chain Data ####################*/}
+          <div className="bg-[#1A202C] col-span-2 lg:col-span-1 p-6 rounded-[24px] space-y-8">
+            <h4 className="text-[24px] font-semibold text-white">On-Chain Data</h4>
+            <div className="space-y-[19px]">
+              <div className="flex justify-between">
+                <span className="text-[#718096] text-[16px]">Owner</span>
+                <span className="text-magenta text-[16px] font-medium">0x57CB...ea4a6</span>
+              </div>
+              <div className="flex justify-between">
+                <span className="text-[#718096] text-[16px]">Contract Address</span>
+                <span className="text-magenta text-[16px] font-medium">0x57CB...ea4a6</span>
+              </div>
+              <div className="flex justify-between">
+                <span className="text-[#718096] text-[16px]">Token ID</span>
+                <span className="text-white text-[16px] font-medium">#1223</span>
+              </div>
+              <div className="flex justify-between">
+                <span className="text-[#718096] text-[16px]">Asset Protocol</span>
+                <span className="text-white text-[16px] font-medium">BSC721</span>
+              </div>
+              <div className="flex justify-between">
+                <span className="text-[#718096] text-[16px]">Asset Public Chain</span>
+                <span className="text-white text-[16px] font-medium">BSC</span>
               </div>
             </div>
           </div>
-          {/* #################### Market Rules ####################*/}
-          <div className="w-full p-6 border border-[#2D3748] rounded-[24px]">
-            <h4 className="text-[24px] text-white font-semibold mb-4">Market Rules</h4>
-            <MarketPlaceRules />
-          </div>
-        </Container>
-      </div>
+        </div>
+        {/* #################### Market Rules ####################*/}
+        <div className="w-full p-6 border border-[#2D3748] rounded-[24px]">
+          <h4 className="text-[24px] text-white font-semibold mb-4">Market Rules</h4>
+          <MarketPlaceRules />
+        </div>
+      </NewLayout>
     </>
   );
 };
